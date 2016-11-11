776a12cfcef1a9aa43fe173f7aea144871c1440f
Jarek Jedlinski <jedlinski.jar@gmail.com>
2016-11-11 21:02:22 +0100

    dodano wybor pizzy i skladnikow

--- gazzola/static/gazzola/js/tomek.js
+++ gazzola/static/gazzola/js/tomek.js
@@ -42,3 +42,82 @@ $.ajaxSetup({
     }
 });
 
+function addTopping(cell) {
+
+    $(cell).parent().children().get(1).innerHTML = $(cell).parent().children().get(1).innerHTML / 1 + 1;
+}
+
+function removeTopping(cell) {
+
+    var quantityCell = $(cell).parent().children().get(1);
+    var newVal = quantityCell.innerHTML / 1 - 1;
+    if (newVal < 0) {
+        quantityCell.innerHTML = 0;
+    } else {
+        quantityCell.innerHTML = newVal;
+    }
+}
+
+function addSmall(pizza) {
+    addToppings(pizza, "1");
+}
+
+function addNormal(pizza) {
+    addToppings(pizza, "2");
+}
+
+function addBig(pizza) {
+    addToppings(pizza, "3");
+}
+
+function refreshToppingsDialog(pizza) {
+    var currentToppings = "" + $("#" + pizza).find(".topping-list").html().replace(/\s /g, '');
+
+    $(".topping-name").each(function (index, entry) {
+
+        var cell = $(entry).parent().children().eq(1);
+
+        if (currentToppings.indexOf(entry.innerHTML) != -1) {
+            $(cell).text(1);
+        } else {
+            $(cell).text(0);
+        }
+    });
+}
+function addToppings(pizza, pizzaSize) {
+
+    refreshToppingsDialog(pizza);
+
+    $("#toppingsDialog")
+        .data("pizza", pizza)
+        .data("pizzaSize", pizzaSize)
+        .dialog();
+}
+
+function addToCart() {
+
+    var dialog = $("#toppingsDialog");
+    var pizza = dialog.data("pizza");
+    var pizzaSize = dialog.data("pizzaSize");
+    var chosenToppings = [];
+
+    $(dialog).find("tr").each(function (index, entry) {
+        var cells = $(entry).find("td");
+        var number = $(cells)[1].innerHTML / 1;
+        for (var i = 0; i < number; i++) {
+            chosenToppings.push($(cells)[0].innerHTML);
+        }
+    });
+
+    $.ajax({
+        type: 'POST',
+        url: '/ajax/save_basket_session/',
+        dataType: 'json',
+        data: {
+            'pizza_name': pizza,
+            'pizza_size': pizzaSize,
+            'toppings': chosenToppings,
+        }
+    });
+}
+
